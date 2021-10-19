0d2125f975ab14f2ae9d49e9b00d11ac301ab9a0
Thiago Billy <thiagobilly27@gmail.com>
2021-10-19 14:53:47 -0300

    inclusão do theme-2

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,13 @@
-let themes = document.querySelectorAll("[data-theme]");
-let themeChosen = document.querySelector(".header__chosen--theme");
+let body = document.querySelector('body')
+let themes = document.querySelectorAll('[data-theme]')
+let themeChosen = document.querySelector('.header__chosen--theme')
 
-themes.forEach(theme => theme.addEventListener('click', () => {
-    let areaChosen = theme.getAttribute('data-theme');
+themes.forEach(theme =>
+  theme.addEventListener('click', () => {
+    let areaChosen = theme.getAttribute('data-theme')
 
-    themeChosen.setAttribute('id', areaChosen);
-    themeChosen.setAttribute('data-theme-chosen', areaChosen);
-
-}))
+    body.setAttribute('id', areaChosen)
+    themeChosen.setAttribute('id', areaChosen)
+    themeChosen.setAttribute('data-theme-chosen', areaChosen)
+  })
+)
